f357255a87c120d2b2a8ce0f7265453cc0148228
Mario Pereira <mons.sidus@gmail.com>
2023-04-25 21:06:03 +0800

    lol

--- app.js
+++ app.js
@@ -5,17 +5,30 @@ const app = express();
 
 //test responses
 app.get('/test', (req, res) => {
-  res.send('Test successful!')
+    res.send('Test successful!')
 })
+app.get('/jesustCrist', (req, res) => {
+    const image = '<img src="/pics/cristhebarbarian.png" alt="Test image">';
+    res.send(`Test successful! ${image}`);
+});
+app.get('/willy', (req, res) => {
+    const image = '<img src="/image.jpg" alt="Test image">';
+    res.send(`Test successful! ${image}`);
+});
+app.get('/cris', (req, res) => {
+    const image = '<img src="/pics/cristhebarbarian.png" alt="Test image">';
+    res.send(`Test successful! ${image}`);
+});
+app.get('/mario', (req, res) => {
+    const image = '<img src="/pics/mariotheextreme.png" alt="Test image">';
+    res.send(`Test successful! ${image}`);
+});
+app.get('/nathan', (req, res) => {
+    const image = '<img src="/pics/nat.png" alt="Test image">';
+    res.send(`Test successful! ${image}`);
+});
 
 // Start the server
 app.listen(8080, () => {
     console.log('Well Done reached port 8080');
 });
-
-// In this example, we 're using the mysql package to create a connection pool to our MySQL database. We then define a route that queries the database for all users and returns the results as a JSON object.
-
-// You 'll need to replace your-mysql-username, your-mysql-password, and your-mysql-database-name with your own MySQL credentials and database name.
-
-// Note that this is just a basic example to get you started.Depending on your needs, you may need to customize this code further to fit your specific use
-// case .
