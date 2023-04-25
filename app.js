7864655e7e354cf75f28432910777629e4a7a873
Mario Pereira <mons.sidus@gmail.com>
2023-04-25 21:24:34 +0800

    yehnah

--- app.js
+++ app.js
@@ -3,28 +3,29 @@ const express = require('express');
 // Create an Express app
 const app = express();
 
+app.use(express.static('public'));
 //test responses
 app.get('/test', (req, res) => {
     res.send('Test successful!')
 })
 app.get('/jesustCrist', (req, res) => {
-    const image = '<img src="/pics/cristhebarbarian.png" alt="Test image">';
+    const image = '<img src="public/pics/cristhebarbarian.png" alt="Test image">';
     res.send(`Test successful! ${image}`);
 });
 app.get('/willy', (req, res) => {
-    const image = '<img src="/image.jpg" alt="Test image">';
+    const image = '<img src="public/pics/willythechad.png" alt="Test image">';
     res.send(`Test successful! ${image}`);
 });
 app.get('/cris', (req, res) => {
-    const image = '<img src="/pics/cristhebarbarian.png" alt="Test image">';
+    const image = '<img src="public/pics/cristhebarbarian.png" alt="Test image">';
     res.send(`Test successful! ${image}`);
 });
 app.get('/mario', (req, res) => {
-    const image = '<img src="/pics/mariotheextreme.png" alt="Test image">';
+    const image = '<img src="public/pics/mariotheextreme.png" alt="Test image">';
     res.send(`Test successful! ${image}`);
 });
 app.get('/nathan', (req, res) => {
-    const image = '<img src="/pics/nat.png" alt="Test image">';
+    const image = '<img src="public/pics/nat.png" alt="Test image">';
     res.send(`Test successful! ${image}`);
 });
 
